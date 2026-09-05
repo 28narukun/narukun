f7409a55c72a0e390c2461b34547575e504f1879
28narukun <ed242111@meiji.ac.jp>
2026-09-05 17:35:59 +0900

    first commit

---
+++ db/migrate/20260729124945_create_tweets.rb
@@ -0,0 +1,12 @@
+class CreateTweets < ActiveRecord::Migration[7.2]
+  def change
+    create_table :tweets do |t|
+      t.string :road
+      t.date :day
+      t.string :address
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
